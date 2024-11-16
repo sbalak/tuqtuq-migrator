c82db896286a580cd56c1d4790810b85099d1d6b
sbalak <bsidharth@live.com>
2024-11-16 17:19:10 +0530

    description added

---
+++ Shopper/Migrations/20241116095423_RestaurantColumnsAdded.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shopper.Migrations
+{
+    /// <inheritdoc />
+    public partial class RestaurantColumnsAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "FoodItems",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDisabled",
+                table: "FoodItems",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "FoodItems",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "FoodItems");
+
+            migrationBuilder.DropColumn(
+                name: "IsDisabled",
+                table: "FoodItems");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "FoodItems");
+        }
+    }
+}
