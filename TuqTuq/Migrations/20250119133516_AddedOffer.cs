944d819b527abce966e9d9522bd7e6a500f881fb
sbalak <sidharth.balakrishnan@outlook.com>
2025-01-19 19:06:48 +0530

    promotions using offer table

---
+++ TuqTuq/Migrations/20250119133516_AddedOffer.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TuqTuq.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedOffer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Offers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Value = table.Column<int>(type: "int", nullable: false),
+                    Threshold = table.Column<int>(type: "int", nullable: false),
+                    UpperLimit = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Offers", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Offers");
+        }
+    }
+}
---
+++ TuqTuq/Models/Offer.cs
@@ -0,0 +1,13 @@
+﻿namespace TuqTuq.Models
+{
+    public class Offer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public int Value { get; set; }
+        public int Threshold { get; set; }
+        public int UpperLimit { get; set; }
+    }
+}
