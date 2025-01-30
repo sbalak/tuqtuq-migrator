b3e566b586b4099d0ad0d4053db202f1b3051b35
sbalak <sidharth.balakrishnan@outlook.com>
2025-01-30 19:53:40 +0530

    update cart with offer

--- TuqTuq/Migrations/20250130142236_UpdateCartWithOffer.Designer.cs
+++ TuqTuq/Migrations/20250130142236_UpdateCartWithOffer.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TuqTuq.Models;
 
@@ -11,9 +12,11 @@ using TuqTuq.Models;
 namespace TuqTuq.Migrations
 {
     [DbContext(typeof(TuqTuqContext))]
-    partial class TuqTuqContextModelSnapshot : ModelSnapshot
+    [Migration("20250130142236_UpdateCartWithOffer")]
+    partial class UpdateCartWithOffer
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TuqTuq/Migrations/20250130142236_UpdateCartWithOffer.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TuqTuq.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCartWithOffer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OfferId",
+                table: "Carts",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Carts_OfferId",
+                table: "Carts",
+                column: "OfferId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Carts_Offers_OfferId",
+                table: "Carts",
+                column: "OfferId",
+                principalTable: "Offers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Carts_Offers_OfferId",
+                table: "Carts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Carts_OfferId",
+                table: "Carts");
+
+            migrationBuilder.DropColumn(
+                name: "OfferId",
+                table: "Carts");
+        }
+    }
+}
